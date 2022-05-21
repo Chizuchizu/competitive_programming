c70c4676bdbc7c2c3097d1c430842d74b8ffbb1d
Ochi Yuma <ochiyuma.ochi@gmail.com>
2022-05-21 21:31:30 +0900

    DP-G

---
+++ AtCoder/dp/g/code.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF32 2147483647 // INT
+#define INF64 9223372036854775807  // long long
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+    if (a < b) {
+        a = b;  // aをbで更新
+        return true;
+    }
+    return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+    if (a > b) {
+            a = b;  // aをbで更新
+            return true;
+        }
+    return false;
+}
+
+int N, M;
+int dep[1001010];
+int ans[1001010];    
+
+
+int main(){
+    cin >> N >> M;
+
+    vector<vector<int>> G(N);
+    vector<int> indegree(N);
+
+    for (int i = 0; i < M; i++){
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+
+        G[a].push_back(b);
+        indegree[b] += 1;
+    }
+
+
+    stack<int> que;
+    vector<int> sorted;
+
+    int root = 0;
+    
+    for (int i = 0; i < N; i++){
+        if (indegree[i] == 0) { 
+            que.push(i);
+        }
+    }
+
+    // que.push(root);
+    while (!que.empty()) {
+        int v = que.top();
+        que.pop();
+
+        sorted.push_back(v);
+        for (int u: G[v]) {
+
+            indegree[u] -= 1;
+
+            if (indegree[u] == 0){
+                que.push(u);
+            }
+        }
+
+    }
+
+    for (int i = 0; i < N; i++){
+        int cu = sorted[i];
+        for (int to: G[cu]) {
+            chmax(
+                    dep[to],
+                    dep[cu] + 1
+                 );
+        }
+    }
+    /*
+    for (int i = 0; i < N; i++){
+        cout << dep[i] << " ";
+    }
+    cout << endl;
+    */
+
+
+    int final_ans = 0;
+
+    for (int i = 0; i < N; i++){
+        final_ans = max(dep[i], final_ans);
+    }
+    cout << final_ans << endl;
+}
